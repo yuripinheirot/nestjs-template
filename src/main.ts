42d14e460c91d7976c91d2bdd7d0126208f01513
Yuri Pinheiro <yuripinheirot@gmail.com>
2025-02-26 11:11:06 -0300

    chore: upgrade dependencies and switch to pnpm

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('query parser', 'extended');
 
   const logger = app.get(Logger);
   app.useLogger(logger);
